2335d65a7327db30b6b909e5c5ad60f77ba82c07
Yuri <yuriakirashiba@gmail.com>
2022-01-02 20:04:39 -0300

    feat: added video url

--- src/entities/lecture.ts
+++ src/entities/lecture.ts
@@ -1,6 +1,13 @@
 export class Lecture {
   readonly description: string
-  constructor (description: string) {
+  readonly videoUrl: string
+
+  constructor (description: string, videoUrl: string) {
     this.description = description
+    this.videoUrl = videoUrl
+  }
+
+  equals (other: Lecture): boolean {
+    return this.description === other.description && this.videoUrl === other.videoUrl
   }
 }
