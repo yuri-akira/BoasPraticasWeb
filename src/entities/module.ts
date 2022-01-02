2c920d2e120bcddc00777806a86ad0e67f08d375
Yuri <yuriakirashiba@gmail.com>
2022-01-02 20:08:49 -0300

    perf: handling module errors

--- src/entities/module.ts
+++ src/entities/module.ts
@@ -1,13 +1,43 @@
 import { Lecture } from './lecture'
 
 export class Module {
-  readonly lectures: Lecture[] = []
+  private readonly lectures: Lecture[] = []
   private readonly name: string
   constructor (name: string) {
     this.name = name
   }
 
+  get numberOfLectures (): number {
+    return this.lectures.length
+  }
+
   add (lecture: Lecture): void {
-    this.lectures.push(lecture)
+    if (!this.includesLectureWithSameName(lecture)) {
+      this.lectures.push(lecture)
+    }
+  }
+
+  private includesLectureWithSameName (lecture: Lecture): boolean {
+    return this.lectures.find(lec => lec.description === lecture.description) !== undefined
+  }
+
+  includes (lecture: Lecture): boolean {
+    return this.lectures.find(lec => lec.equals(lecture)) !== undefined
+  }
+
+  move (lecture: Lecture, to: number): void {
+    if (to > this.lectures.length || to <= 0) {
+      return
+    }
+    const from = this.position(lecture)
+    this.lectures.splice(to - 1, 0, this.lectures.splice(from - 1, 1)[0])
+  }
+
+  position (lecture: Lecture): number {
+    const lectureInModule = this.lectures.find(lec => lec.equals(lecture))
+    if (lectureInModule === undefined) {
+      return undefined
+    }
+    return this.lectures.indexOf(lectureInModule) + 1
   }
 }
