799325f44c1ac2a0faa0672dd73623ec8e09ca75
Yuri <yuriakirashiba@gmail.com>
2022-01-24 18:30:57 -0300

    test: add further meterial

---
+++ test/entities/lecture.spec.ts
@@ -0,0 +1,13 @@
+import { Lecture } from '../../src/entities'
+import { Material } from '../../src/entities/material'
+import { Pdf } from '../../src/entities/pdf'
+
+describe('Lecture', () => {
+  it('should be able to add further material to lectures', () => {
+    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+    const branchingPdf: Material = new Pdf('Branching', 'https://storage/branching.pdf')
+    lecture.add(branchingPdf)
+
+    expect(lecture.includes(branchingPdf)).toBeTruthy()
+  })
+})
